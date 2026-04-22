f112e3862b6051969baf1d4b0a27e7b28e5f9a8b
setyi-diaz <setyi.diaz@udea.edu.co>
2026-04-22 02:06:58 -0500

    El modulo de carga-actualización se modifica para crear y usar arreglos dinámicos

--- CodDesafio2/main.cpp
+++ CodDesafio2/main.cpp
@@ -5,17 +5,27 @@ using namespace std;
 
 int main()
 {
-    Equipo selecciones[3];
+    Equipo* selecciones = nullptr;
+    unsigned int cont = 0;
     try {
-        cargarEstadisticasSelecciones(selecciones);
+        selecciones = cargarEstadisticasSelecciones(cont);
     } catch (const exception& e) {
         cerr << e.what() << "\n";
+        delete [] selecciones;
+        selecciones = nullptr;
         return 1;
     }
-    selecciones[0].setPartidosEmpatados(2);
-    selecciones[1].setPartidosEmpatados(2);
-    selecciones[2].setPartidosEmpatados(2);
-    actualizarEstadisticasSelecciones(selecciones);
 
+    if(selecciones != nullptr){
+        selecciones[0].setPartidosEmpatados(3);
+        selecciones[1].setPartidosEmpatados(3);
+        selecciones[2].setPartidosEmpatados(3);
+        selecciones[3].setPartidosEmpatados(3);
+        selecciones[4].setPartidosEmpatados(3);
+        selecciones[5].setPartidosEmpatados(3);
+    }
+    actualizarEstadisticasSelecciones(selecciones, cont);
+    delete[] selecciones;
+    selecciones = nullptr;
     return 0;
 }
